2b9d814793379c8ab474fe0dc3c2550e739e9210
thevinodpatidar <pvinod813@gmail.com>
2025-11-07 20:50:47 +0530

    Update package.json dependencies, enhance email components with functional exports, and refactor authentication email handling. Upgrade @react-email/components and react-email to their latest versions for improved functionality. Add user type exports in schema.ts for better type inference.

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,3 +1,4 @@
+import { render } from "@react-email/components";
 import { createServerOnlyFn } from "@tanstack/react-start";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
@@ -11,8 +12,17 @@ import {
 import { passkey } from "better-auth/plugins/passkey";
 import { reactStartCookies } from "better-auth/react-start";
 import { db } from "@/db";
+import { TwoFactorOtpEmail } from "@/emails/2fa-otp-verification";
+import { ChangeEmailVerificationEmail } from "@/emails/change-email-verification";
+import { VerificationEmail } from "@/emails/email-verification";
+import { MagicLinkEmail } from "@/emails/magic-link-login";
+import { OrganizationInviteEmail } from "@/emails/organization-invitation";
+import { ResetPasswordEmail } from "@/emails/reset-password";
+import { ResetPasswordOtpEmail } from "@/emails/reset-password-otp";
+import { SigninOtpVerificationEmail } from "@/emails/signin-otp-verification";
 import { env } from "@/env/server";
 import { getUserDefaultOrganizationIdFn } from "@/utils/user-default-organization-id";
+import { resend } from "../resend";
 
 const getAuthConfig = createServerOnlyFn(() => {
   return betterAuth({
@@ -31,25 +41,25 @@ const getAuthConfig = createServerOnlyFn(() => {
       deleteUser: { enabled: true },
       changeEmail: {
         enabled: true,
-        sendChangeEmailVerification: async () => {
-          // await resend.emails.send({
-          //   from: env.BETTER_AUTH_EMAIL_FROM,
-          //   to: user.email, // verification email must be sent to the current user email to approve the change
-          //   subject: "Approve email change",
-          //   react: ChangeEmailVerificationEmail({
-          //     url,
-          //     newEmailAddress: newEmail,
-          //     expiresIn: "24 hours",
-          //   }),
-          //   text: await render(
-          //     ChangeEmailVerificationEmail({
-          //       url,
-          //       newEmailAddress: newEmail,
-          //       expiresIn: "24 hours",
-          //     }),
-          //     { plainText: true }
-          //   ),
-          // });
+        sendChangeEmailVerification: async ({ user, url, newEmail }) => {
+          await resend.emails.send({
+            from: env.BETTER_AUTH_EMAIL_FROM,
+            to: user.email, // verification email must be sent to the current user email to approve the change
+            subject: "Approve email change",
+            react: ChangeEmailVerificationEmail({
+              url,
+              newEmailAddress: newEmail,
+              expiresIn: "24 hours",
+            }),
+            text: await render(
+              ChangeEmailVerificationEmail({
+                url,
+                newEmailAddress: newEmail,
+                expiresIn: "24 hours",
+              }),
+              { plainText: true }
+            ),
+          });
         },
       },
     },
@@ -59,19 +69,17 @@ const getAuthConfig = createServerOnlyFn(() => {
       autoSignIn: false,
       sendOnSignUp: false,
       requireEmailVerification: false,
-      sendResetPassword: async () => {
-        // await resend.emails.send({
-        //   from: env.BETTER_AUTH_EMAIL_FROM,
-        //   to: user.email,
-        //   subject: "Reset your password",
-        //   react: ResetPasswordEmail({ url, expiresIn: "24 hours" }),
-        //   text: await render(
-        //     ResetPasswordEmail({ url, expiresIn: "24 hours" }),
-        //     {
-        //       plainText: true,
-        //     }
-        //   ),
-        // });
+      sendResetPassword: async ({ user, url }) => {
+        await resend.emails.send({
+          from: env.BETTER_AUTH_EMAIL_FROM,
+          to: user.email,
+          subject: "Reset your password",
+          react: ResetPasswordEmail({ url, expiresIn: "24 hours" }),
+          text: await render(
+            ResetPasswordEmail({ url, expiresIn: "24 hours" }),
+            { plainText: true }
+          ),
+        });
       },
     },
     account: {
@@ -101,23 +109,23 @@ const getAuthConfig = createServerOnlyFn(() => {
       sendOnSignUp: true,
       autoSignInAfterVerification: true,
       expiresIn: 600, // 10 minutes
-      sendVerificationEmail: async () => {
-        // await resend.emails.send({
-        //   from: env.BETTER_AUTH_EMAIL_FROM,
-        //   to: user.email,
-        //   subject: "Verify your email address",
-        //   react: VerificationEmail({
-        //     verificationUrl: url,
-        //     expiresIn: "10 minutes",
-        //   }),
-        //   text: await render(
-        //     VerificationEmail({
-        //       verificationUrl: url,
-        //       expiresIn: "10 minutes",
-        //     }),
-        //     { plainText: true }
-        //   ),
-        // });
+      sendVerificationEmail: async ({ user, url }) => {
+        await resend.emails.send({
+          from: env.BETTER_AUTH_EMAIL_FROM,
+          to: user.email,
+          subject: "Verify your email address",
+          react: VerificationEmail({
+            verificationUrl: url,
+            expiresIn: "10 minutes",
+          }),
+          text: await render(
+            VerificationEmail({
+              verificationUrl: url,
+              expiresIn: "10 minutes",
+            }),
+            { plainText: true }
+          ),
+        });
       },
     },
     plugins: [
@@ -129,59 +137,59 @@ const getAuthConfig = createServerOnlyFn(() => {
           digits: 6,
           period: 300, // 5 minutes
           allowedAttempts: 3,
-          async sendOTP() {
-            // await resend.emails.send({
-            //   from: env.BETTER_AUTH_EMAIL_FROM,
-            //   to: user.email,
-            //   subject: "2FA OTP for Next Better Auth Boilerplate",
-            //   react: TwoFactorOtpEmail({
-            //     otpCode: otp,
-            //     expiresIn: "5 minutes",
-            //   }),
-            //   text: await render(
-            //     TwoFactorOtpEmail({
-            //       otpCode: otp,
-            //       expiresIn: "5 minutes",
-            //     }),
-            //     { plainText: true }
-            //   ),
-            // });
+          async sendOTP({ user, otp }) {
+            await resend.emails.send({
+              from: env.BETTER_AUTH_EMAIL_FROM,
+              to: user.email,
+              subject: "2FA OTP for Next Better Auth Boilerplate",
+              react: TwoFactorOtpEmail({
+                otpCode: otp,
+                expiresIn: "5 minutes",
+              }),
+              text: await render(
+                TwoFactorOtpEmail({
+                  otpCode: otp,
+                  expiresIn: "5 minutes",
+                }),
+                { plainText: true }
+              ),
+            });
           },
         },
       }),
       organization({
         cancelPendingInvitationsOnReInvite: true,
-        async sendInvitationEmail() {
-          // const inviteLink = `${env.BETTER_AUTH_URL}/accept-invitation/${data.id}`;
-          // await resend.emails.send({
-          //   from: env.BETTER_AUTH_EMAIL_FROM,
-          //   to: data.email,
-          //   subject: "Invitation to join organization",
-          //   react: OrganizationInviteEmail({
-          //     organizationName: data.organization.name,
-          //     inviteLink,
-          //   }),
-          //   text: await render(
-          //     OrganizationInviteEmail({
-          //       organizationName: data.organization.name,
-          //       inviteLink,
-          //     }),
-          //     { plainText: true }
-          //   ),
-          // });
+        async sendInvitationEmail(data) {
+          const inviteLink = `${env.BETTER_AUTH_URL}/accept-invitation/${data.id}`;
+          await resend.emails.send({
+            from: env.BETTER_AUTH_EMAIL_FROM,
+            to: data.email,
+            subject: "Invitation to join organization",
+            react: OrganizationInviteEmail({
+              organizationName: data.organization.name,
+              inviteLink,
+            }),
+            text: await render(
+              OrganizationInviteEmail({
+                organizationName: data.organization.name,
+                inviteLink,
+              }),
+              { plainText: true }
+            ),
+          });
         },
       }),
       magicLink({
-        sendMagicLink: async () => {
-          // await resend.emails.send({
-          //   from: env.BETTER_AUTH_EMAIL_FROM,
-          //   to: email,
-          //   subject: "Magic Link for Next Better Auth Boilerplate",
-          //   react: MagicLinkEmail({ magicLink: url }),
-          //   text: await render(MagicLinkEmail({ magicLink: url }), {
-          //     plainText: true,
-          //   }),
-          // });
+        sendMagicLink: async ({ email, url }) => {
+          await resend.emails.send({
+            from: env.BETTER_AUTH_EMAIL_FROM,
+            to: email,
+            subject: "Magic Link for Next Better Auth Boilerplate",
+            react: MagicLinkEmail({ magicLink: url }),
+            text: await render(MagicLinkEmail({ magicLink: url }), {
+              plainText: true,
+            }),
+          });
         },
       }),
       emailOTP({
@@ -190,42 +198,42 @@ const getAuthConfig = createServerOnlyFn(() => {
         allowedAttempts: 1,
         disableSignUp: true, // If the user is not registered, they'll be automatically registered. If you want to prevent this, set to true
         sendVerificationOnSignUp: false,
-        async sendVerificationOTP() {
-          // if (type === "sign-in") {
-          //   await resend.emails.send({
-          //     from: env.BETTER_AUTH_EMAIL_FROM,
-          //     to: email,
-          //     subject: "Sign in to Next Better Auth Boilerplate",
-          //     react: SigninOtpVerificationEmail({
-          //       verificationCode: otp,
-          //       expiresIn: "5 minutes",
-          //     }),
-          //     text: await render(
-          //       SigninOtpVerificationEmail({
-          //         verificationCode: otp,
-          //         expiresIn: "5 minutes",
-          //       }),
-          //       { plainText: true }
-          //     ),
-          //   });
-          // } else if (type === "forget-password") {
-          //   await resend.emails.send({
-          //     from: env.BETTER_AUTH_EMAIL_FROM,
-          //     to: email,
-          //     subject: "Reset your password",
-          //     react: ResetPasswordOtpEmail({
-          //       otp,
-          //       expiresIn: "5 minutes",
-          //     }),
-          //     text: await render(
-          //       ResetPasswordOtpEmail({
-          //         otp,
-          //         expiresIn: "5 minutes",
-          //       }),
-          //       { plainText: true }
-          //     ),
-          //   });
-          // }
+        async sendVerificationOTP({ email, type, otp }) {
+          if (type === "sign-in") {
+            await resend.emails.send({
+              from: env.BETTER_AUTH_EMAIL_FROM,
+              to: email,
+              subject: "Sign in to Next Better Auth Boilerplate",
+              react: SigninOtpVerificationEmail({
+                verificationCode: otp,
+                expiresIn: "5 minutes",
+              }),
+              text: await render(
+                SigninOtpVerificationEmail({
+                  verificationCode: otp,
+                  expiresIn: "5 minutes",
+                }),
+                { plainText: true }
+              ),
+            });
+          } else if (type === "forget-password") {
+            await resend.emails.send({
+              from: env.BETTER_AUTH_EMAIL_FROM,
+              to: email,
+              subject: "Reset your password",
+              react: ResetPasswordOtpEmail({
+                otp,
+                expiresIn: "5 minutes",
+              }),
+              text: await render(
+                ResetPasswordOtpEmail({
+                  otp,
+                  expiresIn: "5 minutes",
+                }),
+                { plainText: true }
+              ),
+            });
+          }
         },
       }),
       apiKey(),
